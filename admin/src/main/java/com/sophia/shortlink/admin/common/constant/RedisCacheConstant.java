2f819d5cd33f91c8e90abf41eceb87a386cb64ec
Sophiaa1204 <sihanwangsophia@gmail.com>
2024-03-22 23:27:57 -0400

    add: distributed lock to protect user register

---
+++ admin/src/main/java/com/sophia/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.sophia.shortlink.admin.common.constant;
+
+/**
+ * 短链接喉管Redis缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock_user-register";
+}
--- admin/src/main/java/com/sophia/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/sophia/shortlink/admin/service/impl/UserServiceImpl.java
@@ -2,19 +2,22 @@ package com.sophia.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.sophia.shortlink.admin.common.convention.exception.ClientException;
-import com.sophia.shortlink.admin.dto.req.UserRegisterReqDTO;
-import lombok.RequiredArgsConstructor;
-import org.redisson.api.RBloomFilter;
-import org.springframework.beans.BeanUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sophia.shortlink.admin.common.convention.exception.ClientException;
 import com.sophia.shortlink.admin.dao.entity.UserDO;
 import com.sophia.shortlink.admin.dao.mapper.UserMapper;
+import com.sophia.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.sophia.shortlink.admin.dto.resp.UserRespDTO;
 import com.sophia.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import static com.sophia.shortlink.admin.common.constant.RedisCacheConstant.LOCK_USER_REGISTER_KEY;
 import static com.sophia.shortlink.admin.common.enums.UserErrorCodeEnum.USER_NAME_EXIST;
 import static com.sophia.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAVE_ERROR;
 
@@ -25,6 +28,7 @@ import static com.sophia.shortlink.admin.common.enums.UserErrorCodeEnum.USER_SAV
 @RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -52,11 +56,22 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (!hasUserName(requestParam.getUsername())) {
             throw new ClientException(USER_NAME_EXIST);
         }
-        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
-        if (inserted < 1) {
-            throw new ClientException(USER_SAVE_ERROR);
+        RLock lock = redissonClient.getLock(LOCK_USER_REGISTER_KEY + requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+                if (inserted < 1) {
+                    throw new ClientException(USER_SAVE_ERROR);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        } finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+
+
     }
 
 }
