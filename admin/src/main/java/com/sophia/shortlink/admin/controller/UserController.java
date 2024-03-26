0abf339f23e032809fd58eced2b93fd7d7107c99
Sophiaa1204 <sihanwangsophia@gmail.com>
2024-03-25 23:42:43 -0400

    feat: log out

--- admin/src/main/java/com/sophia/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/sophia/shortlink/admin/controller/UserController.java
@@ -76,4 +76,16 @@ public class UserController {
         return Results.success(userService.checkLogin(username, token));
     }
 
+    /**
+     * 用户退出登录
+     * @param username
+     * @param token
+     * @return
+     */
+    @DeleteMapping("api/short-link/v1/user/logout")
+    public Result<Void> logout(@RequestParam("username") String username, @RequestParam("token") String token) {
+        userService.logout(username,token);
+        return Results.success();
+    }
+
 }
